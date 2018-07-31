0995db7026d63cedc70e87b2a8fbcebd462f4de7
orign <chenzhehao2008666@icloud.com>
2018-07-31 16:56:13 +0800

    公共plugin提取，增加zuul

--- src/main/java/com/czh/cloud/BootOfZuulServer.java
+++ src/main/java/com/czh/cloud/BootOfZuulServer.java
@@ -2,9 +2,13 @@ package com.czh.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class BootOfZuulServer {
 
     public static void main(String[] args) throws Exception {
